79f9033e3a1f9a54caf7e88ee6da612477580429
Ovral4eg <ovral4eg@gmail.com>
2024-10-04 13:58:31 +0300

    disable softCoin animation

--- Assets/Clicker/Scripts/GameScene/View/SoftCoinView.cs
+++ Assets/Clicker/Scripts/GameScene/View/SoftCoinView.cs
@@ -12,29 +12,15 @@ namespace Assets.Clicker.Scripts.GameScene.View
     {
         IDisposable _subscribe;
         [SerializeField] private TextMeshProUGUI _textValue;
-        private Vector3 _originPosition;
 
         public void Bind(GameStateProxy gamestate)
         {
-            _originPosition = transform.position;            
-
-            UpdateValue(gamestate.SoftCoins.CurrentValue, false);
-
-            _subscribe = gamestate.SoftCoins.Skip(1).Subscribe(e => UpdateValue(e,true));
+            _subscribe = gamestate.SoftCoins.Skip(1).Subscribe(e => UpdateValue(e));
         }
 
-        public void UpdateValue(double value, bool doAnimation)
+        public void UpdateValue(double value)
         {
             _textValue.text = $"{StringHelper.GetStringFromValue(value)}";
-
-            if (doAnimation) DoAnimation();
-        }
-
-        public void DoAnimation()
-        {
-            var animationSequence = DOTween.Sequence();
-            animationSequence.Append(transform.DOShakePosition(0.1f, 5, 5));
-            animationSequence.Append(transform.DOMove(_originPosition, 0.1f));
         }
 
         private void OnDestroy()
